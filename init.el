e6ba1f74ac2741ed4706622ec7113a69c208a51b
Morten Hein Tiljeset <morten.tiljeset@prevas.dk>
2019-01-22 07:39:37 +0100

    use ng2 mode

--- init.el
+++ init.el
@@ -332,28 +332,31 @@
 (setq projectile-enable-caching t)
 
 ;; php configuration
-(use-package web-mode
-    :ensure
-  :config
-  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.php?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.ts?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (defun my-web-mode-hook ()
-    "Hooks for Web mode."
-    (setq web-mode-markup-indent-offset 2)
-    (setq web-mode-css-indent-offset 2)
-    (setq web-mode-code-indent-offset 2)
-    )
-  (add-hook 'web-mode-hook  'my-web-mode-hook)
-  )
+;; (use-package web-mode
+;;     :ensure
+;;   :config
+;;   (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.php?\\'" . web-mode))
+;;   ;; (add-to-list 'auto-mode-alist '("\\.ts?\\'" . web-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
+;;   ;; (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+;;   (defun my-web-mode-hook ()
+;;     "Hooks for Web mode."
+;;     (setq web-mode-markup-indent-offset 2)
+;;     (setq web-mode-css-indent-offset 2)
+;;     (setq web-mode-code-indent-offset 2)
+;;     )
+;;   (add-hook 'web-mode-hook  'my-web-mode-hook)
+;;   )
+(use-package typescript-mode :ensure)
+(require 'ng2-mode)
+(csetq typescript-indent-level 2)
 (global-hl-line-mode t)
 (c-set-offset 'substatement-open 0)
